4d92d6519e21baabc56196f30c3530eafe0c3337
Richard Paterson <rocketbootkid@gmail.com>
2017-09-13 22:54:34 +0100

    Added Nationality Flags, Marital Status, Children

--- bin/functions.php
+++ bin/functions.php
@@ -7,9 +7,34 @@ function displayPersona() {
 	echo "<img src='" . $details[4] . "'></img><p>";
 	echo "<font face='Georgia' size=6>" . $details[5] . " " . $details[1] . " " . $details[2] . "</font><p>";
 	//echo "<font face='Georgia' size=6>" . generateName() . "</font><p>";
-	echo "<font face='Georgia' size=4>Born: " . generateDOBAge() . "</font><p>";
-	echo "<font face='Georgia' size=4>Country: " . getCountryName($details[3]) . "</font><p>";	
-
+	
+	// DOB / Age
+	$dob_age = generateDOBAge();
+	echo "<font face='Georgia' size=4>Born: " . $dob_age[0] . " (" . $dob_age[1] . ")</font><p>";
+	
+	// Nationality
+	echo "<img src='images/flags/" . strtolower($details[3]) . ".png' style='padding-top: 0.3em;'> <font face='Georgia' size=4 style='padding-top: 0em;'>" . getNationality($details[3]) . "</font><p>";	
+	
+	// Marital Status
+	$marital_status = getMaritalStatus($dob_age[1], $details[5]);
+	if ($marital_status[0] == "Married") {
+		$marital_text = $marital_status[0] . " for " . $marital_status[1] . " year(s)";
+	} else {
+		$marital_text = $marital_status[0];
+	}
+	echo "<font face='Georgia' size=4>" . $marital_text . "</font><p>";
+	
+	// Children
+	$children = getChildren($marital_status[1]);
+	if ($children == 0) {
+		$kids_text = "";
+	} elseif ($children == 1) {
+		$kids_text = "1 child";
+	} else {
+		$kids_text = $children . " children";
+	}
+	echo "<font face='Georgia' size=4>" . $kids_text . "</font><p>";
+	
 }
 
 function generateName() {
@@ -41,7 +66,7 @@ function generateDOBAge() {
 	$birth_date = date('d/m/Y', $rand_time);
 	$age = calculateAge($birth_date);
 	
-	return $birth_date . " (" . $age . ")";
+	return [$birth_date, $age];
 	
 }
 
@@ -67,9 +92,6 @@ function getUserDetails() {
 	
 	$url = "https://randomuser.me/api/";
 	
-	//echo file_get_contents($url);
-	//var_dump(json_decode(file_get_contents($url)));
-	
 	$json = json_decode(file_get_contents($url));
 	
 	$gender = $json->results[0]->gender;
@@ -83,18 +105,75 @@ function getUserDetails() {
 	
 }
 
-function getCountryName($prefix) {
+function getNationality($prefix) {
 	
-	$country_name = "";
+	$nationality = "";
 	$countries = file('lists/countries.txt');
 	foreach ($countries as $country) {
 		$elements = explode(",", $country);
-		if (trim($elements[1]) == $prefix) {
-			$country_name = $elements[0];
+		if (trim($elements[2]) == $prefix) {
+			$nationality = $elements[1];
+		}
+	}
+	if ($nationality == "") {
+		$nationality = "British";
+	}
+	
+	return $nationality;
+	
+}
+
+function getMaritalStatus($age, $title) {
+	
+	// Likelihood of being married increases from 0.2 at age 22 to 0.9 at age 40, then constant from then on.
+	
+	if ($title == "Ms" || $title == "Miss"  || $title == "Mademoiselle") {
+		$status = "Single";
+	} elseif ($title == "Mrs" || $title == "Madame") {
+		$status = "Married"; 
+	} else {
+		
+		if ($age > 40) {
+			$marital_chance = 0.9;
+		} else {
+			$marital_chance = ((($age-22)/(70-22)) * 0.7) + 0.2;
+		}
+		
+		if (rand(0, 1) < $marital_chance) {
+			$status = "Married";
+		} else {
+			$status = "Single";
 		}
 	}
 	
-	return $country_name;
+	if ($status == "Married") {
+		if ($age > 40) {
+			$marital_chance = 0.9;
+		} else {
+			$marital_chance = ((($age-22)/48) * 0.7) + 0.2;
+		}
+		$duration = rand(1, ($age-22));
+	} else {
+		$duration = 0;
+	}
+	
+	return [$status, $duration];
+	
+}
+
+function getChildren($duration) {
+	
+	$max_kids = 4;
+	
+	if ($duration < $max_kids) {
+		$max = $duration;
+	} else {
+		$max = $max_kids;
+	}
+	
+	return rand(0, $max);
+	
+	
 	
 }
 
